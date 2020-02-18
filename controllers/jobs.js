907c7948174117197aedb0f4a04d68304c2982ba
jmeade11 <hobokenjam@yahoo.com>
2020-02-18 07:49:06 -0500

    Fix HTTP status codes and add next to controllers
    Fixed the status codes for delete and post so the response has 204 and
    201 codes respectively.  Added a down and dirty 404 handler to edit,
    delete and show routes in case there's no document found with the id
    that is provided.  Added next to all the job controllers and passed
    next to a catch method to prevent `UnhandledPromiseRejectionWarning`
    errors.

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 // INDEX
 // GET api/jobs
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   // Use our Job model to find all of the documents
   // in the jobs collection
   // Then send all of the jobs back as json
@@ -14,30 +14,56 @@ router.get('/', (req, res) => {
 
 // SHOW
 // GET api/jobs/5a7db6c74d55bc51bdf39793
-router.get('/:id', (req, res) => {
-  Job.findById(req.params.id).then(job => res.json(job));
+router.get('/:id', (req, res, next) => {
+  Job.findById(req.params.id)
+    .then(job => {
+      if (!job) {
+        res.sendStatus(404);
+      } else {
+        res.json(job);
+      }
+    })
+    .catch(next);
 });
 
 // CREATE
 // POST api/jobs
-router.post('/', (req, res) => {
-  Job.create(req.body).then(job => res.json(job));
+router.post('/', (req, res, next) => {
+  Job.create(req.body)
+    .then(job => res.status(201).json(job))
+    .catch(next);
 });
 
 // UPDATE
 // PUT api/jobs/5a7db6c74d55bc51bdf39793
-router.put('/:id', (req, res) => {
+router.put('/:id', (req, res, next) => {
   Job.findOneAndUpdate({ _id: req.params.id }, req.body, {
     new: true
-  }).then(job => res.json(job));
+  })
+    .then(job => {
+      if (!job) {
+        res.sendStatus(404);
+      } else {
+        res.json(job);
+      }
+    })
+    .catch(next);
 });
 
 // DESTROY
 // DELETE api/jobs/5a7db6c74d55bc51bdf39793
-router.delete('/:id', (req, res) => {
+router.delete('/:id', (req, res, next) => {
   Job.findOneAndDelete({
     _id: req.params.id
-  }).then(job => res.json(job));
+  })
+    .then(job => {
+      if (!job) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch(next);
 });
 
 module.exports = router;
